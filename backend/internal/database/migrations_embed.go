05ed83b5d0408bb4bb6e852b17a9e5b6b25b7b94
vigoordi <VincentKunst298@gmail.com>
2026-07-25 22:52:22 +0800

    keep .gitignore rule

--- backend/internal/database/migrations_embed.go
+++
@@ -1,9 +0,0 @@
-package database
-
-import "embed"
-
-// MigrationsFS embeds the migration files from the migrations directory
-// The migrations folder is at the same level as the database package
-//
-//go:embed migrations
-var MigrationsFS embed.FS
